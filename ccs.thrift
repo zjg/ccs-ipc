8036e1ff81ce490966eb41684579ff82a166e7d9
Doug Rosvick <dlr365@gmail.com>
2012-06-29 16:38:22 -0600

    updated thrift format to include completion chunks

--- ccs.thrift
+++ ccs.thrift
@@ -9,9 +9,56 @@ struct CodeCompletionRequest {
    3: required i32 column;
 }
 
+// todo - handle Optional chunks somehow (can't do recursive structs in thrift)
+
+enum CodeCompletionChunkKind {
+
+   /* note - TypedText is not in this enum since it is a required field
+    * of the candidate
+    */
+   //TypedText,
+   
+   /* these kinds have an associated text string */
+   Text,
+   Placeholder,
+   Informative,
+   CurrentParameter,
+   ResultType,
+   
+   /* these kinds won't have an associated text string */
+   LeftParen,
+   RightParen,
+   LeftBracket,
+   RightBracket,
+   LeftBrace,
+   RightBrace,
+   LeftAngle,
+   RightAngle,
+   Comma,
+   Colon,
+   SemiColon,
+   Equal,
+   Space,
+   Newline,
+}
+
+struct CodeCompletionChunk {
+   1: required CodeCompletionChunkKind kind;
+   2: optional string text;
+}
+
+// todo - annotations ?
+
+struct CodeCompletionCandidate {
+   1: required string typedText;
+   2: optional i32 priority;
+   3: optional string parentContext;
+   4: optional list<CodeCompletionChunk> chunks;
+}
+
 struct CodeCompletionResponse {
    1: optional CodeCompletionRequest request;
-   2: required list<string> results;
+   2: required list<CodeCompletionCandidate> results;
 }
 
 service CCS {
